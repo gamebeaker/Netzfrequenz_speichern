00ee490e5f7de3a7d1303bcc8a3b0f5a52f2e08e
Hanny Hans <Play4freeNew@gmx.de>
2022-09-17 17:04:32 +0200

    First Commit

--- Netzfrequenz_speichern/Netzfrequenz_speichern.py
+++ Netzfrequenz_speichern/Netzfrequenz_speichern.py
@@ -1,1 +1,42 @@
+from keep_alive import keep_alive
+import urllib.request, json, time, os
+
+
+def write_data(AntwortArray):
+    DateiPfad = "./Daten/"+str(time.time())[:4]+".csv"
+    try:
+        Datei = open(DateiPfad,"a")
+    except:
+        print("Datei konnte nicht geoeffnet werden: "+time.localtime())
+        time.sleep(1)
+    try:
+        Datei.write(str(AntwortArray[0])+";"+str(AntwortArray[1])+"\n")
+    except:
+        print("Daten konnten nicht geschrieben werden: "+time.localtime())
+        time.sleep(1)
+    try:
+        Datei.close()
+    except:
+        print("Dateei konnten nicht geschlossen werden: "+time.localtime())
+        time.sleep(1)
+
+def get_data_from_url():
+    UrlAntwort = None
+    while UrlAntwort is None:
+        try: 
+            UrlAntwort = urllib.request.urlopen("https://www.netzfrequenz.info/json/aktuell2.json")
+        except:
+            print("url fehler um "+time.strftime("%Y%m%d%H", time.localtime()))
+            time.sleep(10)
+    AntwortArray = json.loads(UrlAntwort.read())
+    UrlAntwort.close()
+    return AntwortArray
+
+
+keep_alive()
+os.makedirs(os.path.dirname("./Daten/"), exist_ok=True)
+while True:
+    time.sleep(0.7)
+    write_data(get_data_from_url())
+
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Netzfrequenz_speichern
+
+Dieses Python Script Speichert ungefaer jede Sekunde die Netzfrequenz, bereitgestellt durch https://www.netzfrequenz.info/, in einer csv datei. Dies CSV Datei befindet sich im Ordner Daten und hat einen Namen aus Ziffern 9999.csv. Die Ziffern sind die ersten 4 stellen des Datums als Int. (import time; print(time.time())). Die Dateigroesse betraegt zwischen 22MB und 32MB. Das Script hat eine Wartezeit von 0.7 sekunden nach Websitenanfrage und speichern der Werte um auf ungefaehr einen Wert pro Sekunde zu kommen.
+CSV format : 
+Zeitpunkt;Frequenz
+1663425631000;49.983
+
+Problem:
+Das Anfragen des Servers nach den Daten sowie das speicher dieser dauert unterschiedlich lang. Deshalb kann es dazukommen, dass Werte doppelt erfasst oder Sekunden uebersprungen werden.
